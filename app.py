253fdc5548ad576202128a54ca01e5173b803e43
mikecolbert <michael-colbert@uiowa.edu>
2025-04-01 13:20:52 -0500

    basic Flask app using Jinja templates

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 # Mike Colbert 04/01/2025
 
 from flask import Flask
+from flask import render_template
 
-# the all-important app variable:
 app = Flask(__name__)
+
 @app.route("/")
-def hello():
-    return "Hello World!!"
+def home():
+    return render_template('index.html')  # Render the index.html template when accessing the root URL
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5002, debug=True)
